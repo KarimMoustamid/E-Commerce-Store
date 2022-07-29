a31bb07ca4919160dd30c9e15724b7b750b8d9ed
karimmoustamid <moustamid.tech@gmail.com>
2022-07-29 13:14:56 -0500

    Basic Api Done

--- API/Startup.cs
+++ API/Startup.cs
@@ -26,7 +26,9 @@ public class Startup
 
        //- DbContext 
         services.AddDbContext<StoreContext>(options =>
-            options.UseSqlite(Configuration.GetConnectionString("DefaultConnection")));
+        {
+            options.UseSqlite(Configuration.GetConnectionString("DefaultConnection"));
+        });
     }
 
     // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
